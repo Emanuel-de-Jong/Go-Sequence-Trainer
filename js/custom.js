2025bab1d42e11f2948f1d04afe7221ea31ba656
Emanuel-de-Jong <emanueldejong@hotmail.com>
2023-03-06 11:29:21 +0100

    Change color select to random select

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,20 @@
 var custom = {};
 
 
-custom.SGFS = {
-    B: [
-        "B[pd];W[cd];B[pp];W[cp];B[eq];W[do];B[ec];W[de];B[nq];W[qc];B[qd];W[pc];B[od];W[nb];B[qk];W[fp];B[gq];W[io];B[dj];W[cl]",
-        "B[pd];W[cd];B[pp];W[dp];B[ec];W[de];B[cq];W[dq];B[cp];W[cn];B[bn];W[cm];B[bm];W[cl];B[nc];W[qq];B[qp];W[pq];B[nq];W[oq]",
-        "B[pd];W[cd];B[pp];W[dq];B[ed];W[ec];B[fc];W[dc];B[fd];W[cf];B[co];W[qc];B[pc];W[qd];B[qf];W[qe];B[pe];W[rf];B[qb];W[rb]",
-        // "B[pd];W[cp];B[pp];W[cd];B[eq];W[do];B[ec];W[de];B[nq];W[qc];B[qd];W[pc];B[od];W[nb];B[qk];W[fp];B[gq];W[io];B[dj];W[cl]",
-        // "B[pd];W[cp];B[pp];W[dc];B[ep];W[eq];B[fq];W[dq];B[fp];W[cn];B[ce];W[qq];B[pq];W[qp];B[qn];W[qo];B[po];W[rn];B[qr];W[rr]",
-        // "B[pd];W[cp];B[pp];W[dd];B[eq];W[do];B[cc];W[dc];B[cd];W[cf];B[bf];W[cg];B[bg];W[ch];B[nq];W[qc];B[qd];W[pc];B[nc];W[oc]",
-        "B[pd];W[dc];B[dp];W[pp];B[ce];W[ed];B[qq];W[pq];B[qp];W[qn];B[qo];W[po];B[rn];W[qc];B[pc];W[qd];B[qe];W[re];B[rf];W[qf]",
-        "B[pd];W[dc];B[dp];W[pq];B[qo];W[op];B[ce];W[fd];B[qf];W[cn];B[fp];W[ch];B[pl];W[hq];B[jq];W[ho];B[mq];W[jo];B[lo];W[on]",
-        "B[pd];W[dc];B[dp];W[qp];B[oq];W[po];B[ce];W[ed];B[hc];W[nc];B[kc];W[qc];B[pc];W[qd];B[pe];W[pb];B[ob];W[qb];B[oc];W[qf]",
-        "B[pd];W[dd];B[dp];W[pp];B[nq];W[qn];B[pq];W[qq];B[qr];W[oq];B[pr];W[op];B[or];W[qc];B[qd];W[pc];B[nc];W[oc];B[od];W[nb]",
-        // "B[pd];W[dd];B[dp];W[pq];B[qo];W[op];B[cc];W[dc];B[cd];W[cf];B[ce];W[de];B[bf];W[cq];B[dq];W[cp];B[co];W[bo];B[bn];W[cn]",
-        // "B[pd];W[dp];B[pp];W[cd];B[ec];W[de];B[cq];W[dq];B[cp];W[cn];B[bn];W[cm];B[bm];W[cl];B[nc];W[qq];B[qp];W[pq];B[nq];W[oq]",
-        "B[pd];W[dp];B[pp];W[dc];B[cq];W[cp];B[dq];W[ep];B[fq];W[qc];B[qd];W[pc];B[od];W[nb];B[gr];W[cf];B[cl];W[go];B[ci];W[pi]",
-    ],
-    W: [
-        "B[pd];W[dp];B[pp];W[dd];B[fc];W[qq];B[pq];W[qp];B[po];W[rn];B[cf];W[ef];B[ci];W[nc];B[db];W[cc];B[hc];W[gd];B[oc];W[gc];B[gb];W[hd];B[ic];W[id];B[ec];W[ed];B[fq];W[jd];B[cn];W[jb];B[nd];W[ha]",
-        "B[pd];W[dp];B[pq];W[dd];B[cq];W[dq];B[cp];W[do];B[bn];W[qc];B[qd];W[pc];B[od];W[nc];B[rc];W[nd];B[qn];W[nf];B[cm];W[iq]",
-        "B[pd];W[dp];B[qp];W[dd];B[cc];W[dc];B[cd];W[de];B[bf];W[qc];B[pc];W[qd];B[pe];W[rf];B[ic];W[oq];B[cg];W[po];B[qo];W[pn]",
-        "B[qd];W[dd];B[cp];W[pp];B[fq];W[oc];B[pf];W[fc];B[nq];W[pn];B[cf];W[oq];B[cd];W[cc];B[bc];W[cb];B[de];W[bd];B[ce];W[bb]",
-        "B[qd];W[dd];B[dp];W[pp];B[nc];W[cq];B[dq];W[cp];B[cn];W[co];B[do];W[bn];B[qn];W[nq];B[fc];W[df];B[qp];W[qq];B[rq];W[qo]",
-        "B[qd];W[dd];B[dq];W[pp];B[dn];W[oc];B[pf];W[fc];B[cf];W[pn];B[cd];W[cc];B[bc];W[cb];B[de];W[bd];B[ce];W[bb];B[di];W[jq]",
-        // "B[qd];W[dd];B[pp];W[dp];B[cq];W[dq];B[cp];W[do];B[bn];W[qq];B[pq];W[qp];B[po];W[rn];B[iq];W[oc];B[cm];W[pe];B[qe];W[pf]",
-        "B[qd];W[dd];B[pq];W[dq];B[cc];W[cd];B[dc];W[ed];B[fb];W[oc];B[qn];W[cn];B[pc];W[od];B[pf];W[kd];B[gc];W[di];B[jq];W[hq]",
-        "B[qd];W[dd];B[qp];W[dp];B[cq];W[cp];B[dq];W[ep];B[fq];W[oc];B[gr];W[oq];B[pf];W[fc];B[cj];W[cl];B[cg];W[ce];B[gc];W[gb]",
-    ]
-}
+custom.SGFS = [
+    "B[pd];W[cd];B[pp];W[cp];B[eq];W[do];B[ec];W[de];B[nq];W[qc];B[qd];W[pc];B[od];W[nb];B[qk];W[fp];B[gq];W[io];B[dj];W[cl]",
+    "B[pd];W[cd];B[pp];W[dp];B[ec];W[de];B[cq];W[dq];B[cp];W[cn];B[bn];W[cm];B[bm];W[cl];B[nc];W[qq];B[qp];W[pq];B[nq];W[oq]",
+    "B[pd];W[cd];B[pp];W[dq];B[ed];W[ec];B[fc];W[dc];B[fd];W[cf];B[co];W[qc];B[pc];W[qd];B[qf];W[qe];B[pe];W[rf];B[qb];W[rb]",
+];
 
 
 custom.init = function() {
     custom.mistakesElement = document.getElementById("mistakes");
-    custom.colorSelect = document.getElementById("colorSelect");
+    custom.randomSelect = document.getElementById("randomSelect");
     custom.scrambleSelect = document.getElementById("scrambleSelect");
     custom.boardElement = document.getElementById("board");
 
-    custom.colorSelect.addEventListener("change", custom.newBtnClickListener);
+    custom.randomSelect.addEventListener("change", custom.newBtnClickListener);
     custom.scrambleSelect.addEventListener("change", custom.newBtnClickListener);
     document.getElementById("newBtn").addEventListener("click", custom.newBtnClickListener);
     document.getElementById("resetBtn").addEventListener("click", custom.resetBtnClickListener);
@@ -49,48 +26,40 @@ custom.newBtnClickListener = function() {
     custom.mistakes = 0;
     custom.mistakesElement.innerHTML = custom.mistakes;
 
+    custom.lastRandom = custom.random;
+    custom.random = custom.randomSelect.value;
+
     custom.lastScramble = custom.scramble;
     custom.scramble = custom.scrambleSelect.value;
 
-    custom.lastColor = custom.color;
-    custom.color = custom.colorSelect.value;
-
     custom.lastSGF = custom.sgf;
     
-    if (custom.color[1] == "R") {
-        let color = custom.color[0];
-        if (color == "A") {
-            color = custom.randomInt(2) == 1 ? "B" : "W";
-        }
-
+    if (custom.random == "full") {
         do {
-            custom.setSGF(custom.SGFS[color][custom.randomInt(custom.SGFS[color].length)]);
+            custom.setSGF(custom.SGFS[custom.randomInt(custom.SGFS.length)]);
         } while (custom.sgf == custom.lastSGF);
     } else {
-        if (custom.lastColor == custom.color &&
+        if (custom.lastRandom == custom.random &&
                 custom.lastScramble == custom.scramble &&
-                (custom.shuffledSGFs.length - 1) > custom.shuffledSGFsIndex) {
-            custom.shuffledSGFsIndex++;
+                (custom.SGFS.length - 1) > custom.sgfsIndex) {
+            custom.sgfsIndex++;
         } else {
-            if (custom.lastColor == custom.color && custom.lastScramble == custom.scramble) {
+            if (custom.lastRandom == custom.random && custom.lastScramble == custom.scramble) {
                 alert("Cycle finished");
             }
 
-            let sgfs;
-            if (custom.color[0] == "A") {
-                sgfs = custom.SGFS.B;
-                sgfs = sgfs.concat(custom.SGFS.W);
-            } else if (custom.color[0] == "B") {
-                sgfs = custom.SGFS.B;
-            } else if (custom.color[0] == "W") {
-                sgfs = custom.SGFS.W;
-            }
+            custom.sgfsIndex = 0;
 
-            custom.shuffledSGFs = custom.shuffleArray([...sgfs]);
-            custom.shuffledSGFsIndex = 0;
+            if (custom.random == "shuffled") {
+                custom.shuffledSGFs = custom.shuffleArray([...custom.SGFS]);
+            }
         }
 
-        custom.setSGF(custom.shuffledSGFs[custom.shuffledSGFsIndex]);
+        if (custom.random == "none") {
+            custom.setSGF(custom.SGFS[custom.sgfsIndex]);
+        } else if (custom.random == "shuffled") {
+            custom.setSGF(custom.shuffledSGFs[custom.sgfsIndex]);
+        }
     }
 
     custom.createBoard(custom.sgf);
